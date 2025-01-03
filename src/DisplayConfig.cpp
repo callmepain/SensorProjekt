6a7ec4bb569191545d87661e433d70302b59192e
callmepain <beta3001@googlemail.com>
2025-01-03 12:34:40 +0100

    display off verbessert

--- src/DisplayConfig.cpp
+++ src/DisplayConfig.cpp
@@ -1,3 +1,19 @@
 #include "DisplayConfig.h"
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Interner Zustand des Displays
+static bool displayOn = true;
+
+void setDisplayState(bool state) {
+    displayOn = state;
+    if (displayOn) {
+        display.ssd1306_command(SSD1306_DISPLAYON); // Display einschalten
+    } else {
+        display.ssd1306_command(SSD1306_DISPLAYOFF); // Display ausschalten
+    }
+}
+
+bool isDisplayOn() {
+    return displayOn;
+}
--- src/DisplayConfig.h
+++ src/DisplayConfig.h
@@ -6,6 +6,11 @@
 // Display-Einstellungen
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+
 extern Adafruit_SSD1306 display;
+
+// Prototypen für Display-Funktionen
+void setDisplayState(bool state);
+bool isDisplayOn();
 
 #endif
